e0b2c889ff0317c491d3791cca949d2418b5561a
g30079293 <gongshijie3@h-partners.com>
2026-01-08 16:55:44 +0800

    cleancode

--- src/ubsocket/example/test_connect/client.cpp
+++ src/ubsocket/example/test_connect/client.cpp
@@ -19,108 +19,107 @@ int32_t g_port;
 
 static int OsdParseArgs(int argc, char* argv[])
 {
-   if (argc != ARGS_NUM) {
-       printf("invalid args, usage: %s <port>\n", argv[0]);
-       return -1;
-   }
-   g_ip = std::string(argv[1]);
-   g_port = atoi(argv[ARGS_2]);
-   return 0;
+    if (argc != ARGS_NUM) {
+        printf("invalid args, usage: %s <port>\n", argv[0]);
+        return -1;
+    }
+    g_ip = std::string(argv[1]);
+    g_port = atoi(argv[ARGS_2]);
+    return 0;
 }
 
 int SetNonBlocking(int fd)
 {
-   int flags = fcntl(fd, F_GETFL, 0);
-   if (flags == -1) {
-       return -1;
-   }
-   return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) {
+        return -1;
+    }
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
 int SetBlocking(int fd)
 {
-   int flags = fcntl(fd, F_GETFL, 0);
-   if (flags == -1) {
-       return -1;
-   }
-   return fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) {
+        return -1;
+    }
+    return fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
 }
-
 
 static int CreateSocket(int &sockfd, struct sockaddr_in &addr, bool isBlock)
 {
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-   if (sockfd < 0) {
-       perror("socket failed");
-       return -1;
-   }
-   std::cout << "socket" << std::endl;
-   if (isBlock) {
-       SetBlocking(sockfd);
-   } else {
-       SetNonBlocking(sockfd);
-   }
-   addr.sin_family = AF_INET;
-   addr.sin_port = htons(g_port);
-   inet_pton(AF_INET, g_ip.c_str(), &addr.sin_addr);
-   return 0;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket failed");
+        return -1;
+    }
+    std::cout << "socket" << std::endl;
+    if (isBlock) {
+        SetBlocking(sockfd);
+    } else {
+        SetNonBlocking(sockfd);
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(g_port);
+    inet_pton(AF_INET, g_ip.c_str(), &addr.sin_addr);
+    return 0;
 }
 
 static int Connect(bool isBlock)
 {
-   std::cout << "Test Connect " << (isBlock ? "[block] mode" : "[non-block] mode") << std::endl;
-   int sockfd = -1;
-   struct sockaddr_in addr {};
-   if (CreateSocket(sockfd, addr, isBlock) < 0) {
-       return -1;
-   }
+    std::cout << "Test Connect " << (isBlock ? "[block] mode" : "[non-block] mode") << std::endl;
+    int sockfd = -1;
+    struct sockaddr_in addr {};
+    if (CreateSocket(sockfd, addr, isBlock) < 0) {
+        return -1;
+    }
 
-   std::cout << "connect isBlock " << isBlock << ", sockfd fd " << sockfd << ", server ip:port " << g_ip << ":"
-             << g_port << std::endl;
-   int ret = connect(sockfd, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr));
-   if (ret != 0) {
-       perror("connect failed");
-       return -1;
-   }
-   printf("connect ret %d, errno %d, %s\n", ret, errno, strerror(errno));
+    std::cout << "connect isBlock " << isBlock << ", sockfd fd " << sockfd << ", server ip:port " << g_ip << ":"
+              << g_port << std::endl;
+    int ret = connect(sockfd, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr));
+    if (ret != 0) {
+        perror("connect failed");
+        return -1;
+    }
+    printf("connect ret %d, errno %d, %s\n", ret, errno, strerror(errno));
 
-   int32_t buf = 1;
-   ret = write(sockfd, &buf, sizeof(int32_t));
-   if (ret != sizeof(int32_t)) {
-       perror("write failed");
-       return -1;
-   }
-   std::cout << "write to server: " << buf << std::endl;
+    int32_t buf = 1;
+    ret = write(sockfd, &buf, sizeof(int32_t));
+    if (ret != sizeof(int32_t)) {
+        perror("write failed");
+        return -1;
+    }
+    std::cout << "write to server: " << buf << std::endl;
 
-   sleep(1);
+    sleep(1);
 
-   ret = read(sockfd, &buf, sizeof(int32_t));
-   if (ret != sizeof(int32_t)) {
-       perror("read failed");
-       return -1;
-   }
-   std::cout << "read from server: " << buf << std::endl;
+    ret = read(sockfd, &buf, sizeof(int32_t));
+    if (ret != sizeof(int32_t)) {
+        perror("read failed");
+        return -1;
+    }
+    std::cout << "read from server: " << buf << std::endl;
 
-   ret = shutdown(sockfd, SHUT_RDWR);
-   if (ret != 0) {
-       perror("shutdown failed");
-       return -1;
-   }
-   std::cout << "shutdown" << std::endl;
+    ret = shutdown(sockfd, SHUT_RDWR);
+    if (ret != 0) {
+        perror("shutdown failed");
+        return -1;
+    }
+    std::cout << "shutdown" << std::endl;
 
-   ret = close(sockfd);
-   if (ret != 0) {
-       perror("close failed");
-       return -1;
-   }
-   std::cout << "close" << std::endl;
-   return 0;
+    ret = close(sockfd);
+    if (ret != 0) {
+        perror("close failed");
+        return -1;
+    }
+    std::cout << "close" << std::endl;
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
-   OsdParseArgs(argc, argv);
-   Connect(true);
-   sleep(1);
-   Connect(false);
+    OsdParseArgs(argc, argv);
+    Connect(true);
+    sleep(1);
+    Connect(false);
 }
