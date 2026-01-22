1c5a5da594c51912e60a8a11484312e9323638cd
qinjiaqi77922 <qinjiaqi6@h-partners.com>
2026-01-22 16:57:58 +0800

    头文件

--- src/hcom/umq/stub/urma/ub_get_clock.h
+++ src/hcom/umq/stub/urma/ub_get_clock.h
@@ -11,4 +11,14 @@
 #include "urma_stub.h"
 #define get_cpu_mhz get_cpu_mhz_stub
 
+#define CLOCK_SIZE_OF_INT_STUB (32)
+#define CLOCK_SIZE_OF_INT CLOCK_SIZE_OF_INT_STUB
+
+uint64_t get_cycles_stub(void)
+{
+    uint64_t cycle = 1;
+    return cycle;
+}
+#define get_cycles get_cycles_stub
+
 #endif
