c22b5ad9ad5717686d219cbdbb148be79171c64a
zbh1957282580 <zhangbohao6@h-partners.com>
2026-01-28 10:21:19 +0800

    update: 更新文件 urpc_util_test.cpp
    Signed-off-by: zbh1957282580 <zhangbohao6@h-partners.com>

--- src/ubsocket/unit_test/urpc_util_test.cpp
+++ src/ubsocket/unit_test/urpc_util_test.cpp
@@ -33,15 +33,15 @@ TEST_F(UrpcUtilTest, Rand)
     int x = 0;
     int ret = 0;
 
-    ret = ubsocket::urpc_rand_generate((uint8_t *)&x, sizeof(x));
+    ret = urpc_rand_generate((uint8_t *)&x, sizeof(x));
     EXPECT_EQ(ret, 0);
     EXPECT_NE(x, 0);
 
     MOCKER_CPP(&ubsocket::urpc_rand_generate).stubs().will(invoke(FakeRand));
-    ret = ubsocket::urpc_rand_generate((uint8_t *)&x, 1);
+    ret = urpc_rand_generate((uint8_t *)&x, 1);
     EXPECT_EQ(ret, -1);
 
-    ret = ubsocket::urpc_rand_generate((uint8_t *)&x, 4);
+    ret = urpc_rand_generate((uint8_t *)&x, 4);
     EXPECT_EQ(ret, 0);
     EXPECT_EQ(x, 0x11112211);
 }
